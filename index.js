82bdd61255356cf3f5a048b2db5064d8ff75ec3d
dismas <afubwa@gmail.com>
2023-01-24 16:06:47 +0400

    Javascript Dom contentloaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    const exercise = document.getElementById("text");
+  
+    exercise.textContent = "This is really cool!";
+  });
